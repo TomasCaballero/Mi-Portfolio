5964419b1b03064b90260a88bffee393158a6cea
Tomas Caballero <caballerotomas97@gmail.com>
2023-06-05 22:14:19 -0300

    cambioSpinner

--- src/Components/PaginaCarga.jsx
+++ src/Components/PaginaCarga.jsx
@@ -5,6 +5,7 @@ const PaginaCarga = () => {
             <div className="textoYSpinner">
                 <img src="src\assets\bg\spinnerCarga.gif" alt="Spinner carga" />
                 <h5>Cargando Portfolio...</h5>
+                
             </div>
             <img src="src\assets\bg\gifCarga2.gif" alt="Imagen de carga" className="imgCarga"/>
         </div>
